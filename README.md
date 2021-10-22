9f6ef35348d3ba2b5c675f2c58f4ae4b83c35a61
Dmytro Mishchenko <dmytromishchenko1@gmail.com>
2021-10-22 14:49:45 +0300

    Added docker-compose file and wrote in README how to start app

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Hotel
+To start app you need to run docker-compose.yml with comand docker-compose up
+After that you have to run yarn start
+Than you can go to simple swagger documentation to see possible endpoints localhost:3000/api
+Than you can use api 
--- src/hotel/hotel.service.spec.ts
+++ src/hotel/hotel.service.spec.ts
@@ -1,8 +1,13 @@
+import { HttpService } from '@nestjs/axios';
+import { ConfigService } from '@nestjs/config';
 import { Test, TestingModule } from '@nestjs/testing';
 import { getRepositoryToken } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { Hotel } from './entities/hotel.entity';
 import { HotelService } from './hotel.service';
+import { from } from 'rxjs'
+import { plainToClass } from 'class-transformer';
+import { Booking } from 'src/booking/entities/booking.entity';
 
 const mockedData: Hotel[] = [{
   id: 1,  
@@ -30,11 +35,12 @@ const mockedData: Hotel[] = [{
 
 describe('HotelService', () => {
   let service: HotelService;
-  const axios = require('axios');
-  jest.mock('axios');
+  let httpService: HttpService;
+  // const axios = require('axios');
+  // jest.mock('axios');
 
   const mockUserRepo = {
-    create: jest.fn().mockImplementation(dto => dto),
+    create: jest.fn().mockImplementation(dto => plainToClass(Booking, {id: 1, ...dto})),
     save: jest.fn().mockImplementation(booking => Promise.resolve({id: 1, ...booking})),
     find: jest.fn().mockImplementation(() => Promise.resolve(mockedData)),
   };
@@ -43,15 +49,23 @@ describe('HotelService', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       
-      providers: [HotelService,
-      {
+      providers: [
+        HotelService,
+        HttpService,
+        ConfigService,
+        {
           provide: getRepositoryToken(Hotel),
           useValue: mockUserRepo,
         },
       ],
+    }).overrideProvider(HttpService).useValue({
+      get: jest.fn().mockReturnValue(from([{data: {items: [{title: 'Test hotel', id: 'abcdef', distance: 50}]}}]))
+    }).overrideProvider(ConfigService).useValue({
+      get: (key) => key
     }).compile();
 
     service = module.get<HotelService>(HotelService);
+    httpService = module.get<HttpService>(HttpService);
   });
 
   it('returns the title of the first album', async () => {
@@ -63,10 +77,16 @@ describe('HotelService', () => {
         bookings: []        
       }
     }
-    axios.get.mockResolvedValue(mockedResponse);
-    const hotelService = require('./hotel.service');
-    hotelService.findAllFromAPI();
-    expect(axios.get).toHaveBeenCalled()
+    // axios.get.mockResolvedValue(mockedResponse);
+    // const hotelService = require('./hotel.service');
+    const result = await service.findAllFromAPI(42, 42);
+    expect(httpService.get).toHaveBeenCalledWith('https://discover.search.hereapi.com/v1/discover?at=42,42&q=hotel&apiKey=API_KEY');
+    expect(result).toEqual([{
+      bookings: [],
+      distanceToCenter: 50,
+      distanceToCenterUnit: "m",
+      title: 'Test hotel'
+    }]);
   });
 
   it('should be defined', () => {
